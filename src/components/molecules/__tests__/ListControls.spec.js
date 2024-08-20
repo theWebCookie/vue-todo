ba10f144ef81429cec39e7cdea1381cfc8d86a03
Wojciech Jablonski <w.jablonski@godeltech.com>
2024-08-20 14:03:39 +0200

    test: Add test for handling filter & clear button click in ListControls component

--- src/components/molecules/__tests__/ListControls.spec.js
+++ src/components/molecules/__tests__/ListControls.spec.js
@@ -2,6 +2,8 @@ import { shallowMount } from '@vue/test-utils';
 import { describe, it, expect, beforeEach } from '@jest/globals';
 import { createStore } from 'vuex';
 import ListControls from '../ListControls.vue';
+import { jest } from '@jest/globals';
+import { render, fireEvent } from '@testing-library/vue';
 
 const styles = {
   controls: 'controls',
@@ -16,14 +18,21 @@ const numberOfTasksLeft = 5;
 describe('ListControls.vue', () => {
   let store;
   let getters;
+  let actions;
 
   beforeEach(() => {
+    actions = {
+      setFilter: jest.fn(),
+      clearCompletedTasks: jest.fn(),
+    };
+
     getters = {
       tasksLeft: () => numberOfTasksLeft,
     };
 
     store = createStore({
       getters,
+      actions,
     });
   });
 
@@ -52,5 +61,29 @@ describe('ListControls.vue', () => {
 
     const taskCountMessage = wrapper.find('span').text();
     expect(taskCountMessage).toBe(`${numberOfTasksLeft} tasks left`);
+  });
+
+  it('dispatches the correct action when a filter button is clicked', async () => {
+    const { getByText } = render(ListControls, {
+      global: {
+        plugins: [store],
+      },
+    });
+
+    const activeFilterButton = getByText('Active');
+    await fireEvent.click(activeFilterButton);
+    expect(actions.setFilter).toHaveBeenCalledWith(expect.any(Object), 'active');
+  });
+
+  it('dispatches the clearCompletedTasks action when the clear completed button is clicked', async () => {
+    const { getByText } = render(ListControls, {
+      global: {
+        plugins: [store],
+      },
+    });
+
+    const clearButton = getByText('Clear Completed');
+    await fireEvent.click(clearButton);
+    expect(actions.clearCompletedTasks).toHaveBeenCalled();
   });
 });
